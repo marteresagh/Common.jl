65a604540aab7e5755208a322d117e3185ffca28
marteresagh <marteresa28@gmail.com>
2020-10-24 18:51:44 +0200

    add test delaunay

--- src/Geometry/delaunay.jl
+++ src/Geometry/delaunay.jl
@@ -2,5 +2,5 @@ function delaunay_triangulation(points::Lar.Points)
     V = convert(Lar.Points,points')
     mesh = delaunay(V);
     DT = [mesh.simplices[c,:] for c in 1:size(mesh.simplices,1)]
-    return DT
+    return sort.(DT)
 end
--- test/delaunay.jl
+++ test/delaunay.jl
@@ -1,33 +1,64 @@
 @testset "Delaunay" begin
 
-	@testset "one tetrahedron" begin
-		P = [
-			0.0 1.0 0.0 0.0
-			0.0 0.0 1.0 0.0
-			0.0 0.0 0.0 1.0
-		];
-		DT = delaunay_triangulation(P)
-		@test DT == [ [1, 2, 3, 4] ]
+	@testset "2D" begin
+		@testset "one tetrahedron" begin
+			P = [
+				0.0 1.0 0.0
+				0.0 0.0 1.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test DT == [ [1, 2, 3] ]
+		end
+
+		@testset "two tetrahedron" begin
+			P = [
+				0.0 1.0 0.0 2.0
+				0.0 0.0 1.0 0.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test DT == [ [1, 2, 3], [2, 3, 4] ]
+		end
+
+		@testset "cube" begin
+			P = [
+				0.0 1.0 0.0 1.0
+				0.0 0.0 1.0 1.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test length(DT) == 2
+		end
 	end
 
-	@testset "two tetrahedron" begin
-		P = [
-			0.0 1.0 0.0 0.0 2.0
-			0.0 0.0 1.0 0.0 0.0
-			0.0 0.0 0.0 1.0 0.0
-		];
-		DT = delaunay_triangulation(P)
-		@test DT == [ [1, 2, 3, 4], [2, 3, 4, 5] ]
-	end
+	@testset "3D" begin
+		@testset "one tetrahedron" begin
+			P = [
+				0.0 1.0 0.0 0.0
+				0.0 0.0 1.0 0.0
+				0.0 0.0 0.0 1.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test DT == [ [1, 2, 3, 4] ]
+		end
 
-	@testset "cube" begin
-		P = [
-			0.0 1.0 0.0 1.0 0.0 1.0 0.0 1.0
-			0.0 0.0 1.0 1.0 0.0 0.0 1.0 1.0
-			0.0 0.0 0.0 0.0 1.0 1.0 1.0 1.0
-		];
-		DT = delaunay_triangulation(P)
-		@test length(DT) == 6
+		@testset "two tetrahedron" begin
+			P = [
+				0.0 1.0 0.0 0.0 2.0
+				0.0 0.0 1.0 0.0 0.0
+				0.0 0.0 0.0 1.0 0.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test DT == [ [1, 2, 3, 4], [2, 3, 4, 5] ]
+		end
+
+		@testset "cube" begin
+			P = [
+				0.0 1.0 0.0 1.0 0.0 1.0 0.0 1.0
+				0.0 0.0 1.0 1.0 0.0 0.0 1.0 1.0
+				0.0 0.0 0.0 0.0 1.0 1.0 1.0 1.0
+			];
+			DT = Common.delaunay_triangulation(P)
+			@test length(DT) == 6
+		end
 	end
 
 end
