66f749775dd9e4784f39135b9a812a68af37e88b
marteresagh <marteresa28@gmail.com>
2020-10-29 17:14:30 +0100

    update

---
+++ examples/keyborad_event.jl
@@ -0,0 +1,15 @@
+using Common
+
+inputBuffer = monitorInput()
+output = Int64[]
+for i in 1:100
+    if isready(inputBuffer) && take!(inputBuffer) == 'q'
+        break
+    end
+    push!(output,i)
+    print('\r', i)
+    sleep(0.1)
+end
+
+@show output
+println("\nPlease have a nice day!")
