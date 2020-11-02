1822c88234fc3399462b9c5a201e8c51573324b6
marteresagh <marteresa28@gmail.com>
2020-11-02 11:05:19 +0100

    fix outliers bug

--- src/Geometry/outliers.jl
+++ src/Geometry/outliers.jl
@@ -1,26 +1,23 @@
 function relative_density_points(PC::PointCloud, current_inds::Array{Int64,1}, k::Int64)
 	points = PC.coordinates[:,current_inds]
-	_, points_inds = remove_double_verts(PC.coordinates[:,current_inds],2)
-	duplicate = setdiff(current_inds,points_inds)
+	npoints = length(current_inds)
 
 	kdtree = NearestNeighbors.KDTree(points)
-	idxs, dists = NearestNeighbors.knn(kdtree, points, k+1, true,  i -> i in duplicate)
 
-	density = Float64[]
-	for i in 1:length(current_inds)
-		rho = sum(dists[i])/k
-		push!(density,1/rho)
+	idxs = Array{Array{Int64,1},1}(undef,npoints)
+	dists = Array{Array{Float64,1},1}(undef,npoints)
+	for i in 1:npoints
+		idx, dist = NearestNeighbors.knn(kdtree, points[:,i], k, true, t -> t == i)
+		idxs[i] = idx
+		dists[i] = dist
 	end
 
-	AVGRelDensity = Float64[]
-	for i in 1:length(current_inds)
-		rel = density[i]/((1/k)*sum(density[idxs[i]]))
-		push!(AVGRelDensity,rel)
-	end
+	density = [k/sum(dists[i]) for i in 1:npoints]
+
+	AVGRelDensity = [density[i]/(sum(density[idxs[i]])/k) for i in 1:npoints]
 
 	return density,AVGRelDensity
 end
-
 
 function outliers(PC::PointCloud, current_inds::Array{Int64,1}, k::Int64)
 	density,AVGRelDensity = relative_density_points(PC, current_inds, k)
