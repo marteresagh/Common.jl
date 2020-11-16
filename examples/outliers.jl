eec3d1ca755176b3b62e9c496f93a725b755a3e3
marteresagh <marteresa28@gmail.com>
2020-11-16 16:04:17 +0100

    fix minor example

--- examples/outliers.jl
+++ examples/outliers.jl
@@ -2,12 +2,12 @@ using Common
 using Visualization
 using FileManager
 
-source = "examples\\muriAngolo.las"
+source = "examples/muriAngolo.las"
 PC = FileManager.las2pointcloud(source)
 PC2D = PointCloud(PC.coordinates[1:2,:])
 k = 10
 outliers = Common.outliers(PC2D, [1:PC2D.n_points...], k)
-tofit = setdiff([1:PC2D.n_points...],outliers)
+tofit = setdiff([1:PC2D.n_points...], outliers)
 
 GL.VIEW([
 			GL.GLPoints(convert(Lar.Points,PC2D.coordinates[:,tofit]'),GL.COLORS[12]),
