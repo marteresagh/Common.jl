8d64ffb33dec2717b972011b4938f9231ac5be96
marteresagh <marteresa28@gmail.com>
2020-11-19 16:04:22 +0100

    fix example

--- examples/plane.jl
+++ examples/plane.jl
@@ -17,13 +17,10 @@ volume = Common.plane2model(plane,thickness,aabb)
 matrix = Common.orthonormal_basis(-0.7071067811865476, 0.7071067811865476, 0.0)
 
 GL.VIEW([
-    #GL.GLPoints(convert(Lar.Points,p1'),GL.COLORS[1])
-	#GL.GLPoints(convert(Lar.Points,p2'),GL.COLORS[2])
-	#GL.GLGrid(Common.apply_matrix(Common.matrix4(matrix),volume[1]),volume[2])
+    GL.GLPoints(convert(Lar.Points,p1'),GL.COLORS[1])
+	GL.GLPoints(convert(Lar.Points,p2'),GL.COLORS[2])
 	GL.GLGrid(volume[1],volume[2])
 	GL.GLGrid(octree[1],octree[2])
-	#Visualization.helper_axis(plane.matrix)...
-	#Visualization.helper_axis(plane.matrix)...
 	Visualization.helper_axis(plane.matrix)...
-	#GL.GLFrame
+	GL.GLFrame
 ]);
