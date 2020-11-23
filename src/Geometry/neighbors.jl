b9a159ac5ad3aee374af42a37721bff081f72da0
marteresagh <marteresa28@gmail.com>
2020-11-23 12:11:07 +0100

    fix strings

--- src/Geometry/neighbors.jl
+++ src/Geometry/neighbors.jl
@@ -1,29 +1,29 @@
 """
-neighborhood(  kdtree,
-			   points::Lar.Points,
-			   seeds::Array{Int64,1},
-			   visitedverts::Array{Int64,1},
-			   threshold::Float64,
-			   k=10::Int64
-		    )
-
-Create the neighborhood of the i-th point defined in seeds.
-"""
-function neighborhood(	kdtree,
+	neighborhood(	kdtree::NNTree{V},
 	 					points::Lar.Points,
 		 				seeds::Array{Int64,1},
 						visitedverts::Array{Int64,1},
 						threshold::Float64,
 						k=10::Int64
-					 )
+					 ) where V <: AbstractVector
+
+Return the neighborhood of seed points, removing all points already visited and too distant.
+"""
+function neighborhood(	kdtree::NNTree{V},
+	 					points::Lar.Points,
+		 				seeds::Array{Int64,1},
+						visitedverts::Array{Int64,1},
+						threshold::Float64,
+						k=10::Int64
+					 ) where V <: AbstractVector
+
 
 	idxs, dists = NearestNeighbors.knn(kdtree, points[:,seeds], k, true, i -> i in visitedverts)
 
-	#TODO da migliorare questa parte
 	neighborhood = Int[]
 
 	for i in 1:length(idxs)
-		filter = [dist<=threshold for dist in dists[i]]
+		filter = [dist<=threshold for dist in dists[i]] # remove too distant points
 		union!(neighborhood,idxs[i][filter])
 	end
 
