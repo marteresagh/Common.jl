379cd21b4aa2c69a97525585d977bab77f7b62ae
marteresagh <marteresa28@gmail.com>
2020-12-09 11:03:39 +0100

    fix minor

--- examples/debug.jl
+++ examples/debug.jl
@@ -7,15 +7,9 @@ model = Common.volume2LARmodel(volume)
 aabb = AABB(0.5,-0.5,0.5,-0.5,0.5,-0.5)
 T,ET,FT = getmodel(aabb)
 
-plane = Plane(0,1,0,0)
-normal = [0,1.,0]
-centroid = normal*0
-rot = Matrix(Common.orthonormal_basis(0,1.,0)')
-matrix = Common.matrix4(rot)
-matrix[1:3,4] = centroid
+plane = Plane(0,0,1,0)
 model = Common.plane2model(plane, 1., aabb)
 matrix = Common.box_new_coords_system(model)
-
 V,EV,FV = model
 
 GL.VIEW(
