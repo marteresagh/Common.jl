5f3665ab5ba80076c498d40d7742caf207e0dcb6
marteresagh <marteresa28@gmail.com>
2020-12-15 09:52:51 +0100

    add boundary function

--- examples/debug.jl
+++ examples/debug.jl
@@ -1,22 +1,6 @@
 using Common
 using Visualization
 
+points = rand(2,100)
 
-volume = Volume([1,1.,1],[0,0,0.],[0.,0,0])
-model = Common.volume2LARmodel(volume)
-aabb = AABB(0.5,-0.5,0.5,-0.5,0.5,-0.5)
-T,ET,FT = getmodel(aabb)
-
-plane = Plane(0,0,1,0)
-model = Common.plane2model(plane, 1., aabb)
-matrix = Common.box_new_coords_system(model)
-V,EV,FV = model
-
-GL.VIEW(
-    [
-    GL.GLGrid(V,EV),
-    #GL.GLGrid(T,ET),
-    Visualization.helper_axis(Common.matrix4(matrix))...,
-    #GL.GLFrame
-    ]
-)
+FV = Common.de
---
+++ src/Geometry/boundary.jl
@@ -0,0 +1,39 @@
+function FV2EV( v )
+	edges = [
+		[v[1], v[2]], [v[1], v[3]], [v[2], v[3]]]
+end
+
+function CV2FV( v )
+	faces = [
+		[v[1], v[2], v[3], v[4]], [v[5], v[6], v[7], v[8]],
+		[v[1], v[2], v[5], v[6]], [v[3], v[4], v[7], v[8]],
+		[v[1], v[3], v[5], v[7]], [v[2], v[4], v[6], v[8]]]
+end
+
+function CV2EV( v )
+	edges = [
+		[v[1], v[2]], [v[3], v[4]], [v[5], v[6]], [v[7], v[8]], [v[1], v[3]], [v[2], v[4]],
+		[v[5], v[7]], [v[6], v[8]], [v[1], v[5]], [v[2], v[6]], [v[3], v[7]], [v[4], v[8]]]
+end
+
+
+
+function get_boundary_edges(V::Lar.Points,FV::Lar.Cells)
+	EV = convert(Array{Array{Int64,1},1}, collect(Set(cat(map(FV2EV,FV)))))
+
+	# M_0 = K(VV)
+	M_1 = K(EV)
+	M_2 = K(FV)
+
+	# ∂_1 = M_0 * M_1'
+	∂_2 = (M_1 * M_2') .÷ 2 #	.÷ sum(M_1,dims=2)
+	# s = sum(M_2,dims=2)
+	# ∂_3 = (M_2 * M_3')
+	# ∂_3 = ∂_3 ./	s
+	# ∂_3 = ∂_3 .÷ 1	#	.÷ sum(M_2,dims=2)
+
+	S1 = sum(∂_2,dims=2)
+	inner = [k for k=1:length(S1) if S1[k]==2]
+	outer = setdiff(collect(1:length(EV)), inner)
+	return EV[outer]
+end
