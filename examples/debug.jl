e60cce7c7e83379beacfcb23a8cb3275a469e014
marteresagh <marteresa28@gmail.com>
2020-12-16 14:59:15 +0100

    batch update

--- examples/debug.jl
+++ examples/debug.jl
@@ -13,3 +13,41 @@ GL.VIEW([
 	GL.GLGrid(points,EV,GL.COLORS[12],1.0)
 	#GL.GLFrame2
 ]);
+
+
+################# DEBUG Delaunay
+using AlphaStructures
+
+points = rand(3,100000)
+
+npoints = 10000
+x = rand(npoints)
+y = zeros(npoints)
+z = rand(npoints)
+
+wall_XZ = convert(Lar.Points, hcat(x,y,z)')
+
+x = zeros(npoints)
+y = rand(npoints)
+z = rand(npoints)
+
+wall_YZ = convert(Lar.Points, hcat(x,y,z)')
+
+points = hcat(wall_XZ,wall_YZ)
+
+using Detection
+source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/MURI"
+INPUT_PC = Detection.source2pc(source,1)
+
+points = Common.apply_matrix(Lar.t(-Common.centroid(INPUT_PC.coordinates)...),INPUT_PC.coordinates)
+
+DT = delaunayTriangulation(points)
+
+FV = Common.delaunay_triangulation(points)
+
+GL.VIEW([
+    GL.GLPoints(convert(Lar.Points,points'),GL.COLORS[1])
+	#GL.GLGrid(points,FV,GL.COLORS[2],1.0)
+	GL.GLGrid(points,FV,GL.COLORS[12],1.0)
+	#GL.GLFrame2
+]);
