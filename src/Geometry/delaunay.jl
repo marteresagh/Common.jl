05b6545ad4341b8fb10c4fe95dc48e651645450f
marteresagh <marteresa28@gmail.com>
2021-02-07 16:18:08 +0100

    add translate in DT

--- src/Geometry/delaunay.jl
+++ src/Geometry/delaunay.jl
@@ -4,7 +4,9 @@
 Delaunay triangulation of points in d-dimensional Euclidean space.
 """
 function delaunay_triangulation(points::Lar.Points)::Lar.Cells
-    V = convert(Lar.Points,points')
+    centroid = centroid(points)
+    T = Common.apply_matrix(Lar.t(-centroid...),points)
+    V = convert(Lar.Points,T')
     mesh = delaunay(V);
     DT = [mesh.simplices[c,:] for c in 1:size(mesh.simplices,1)]
     return sort.(DT)
