3e9f10a7aa43a06b1cf8dd8a1846f762d516a6a3
marteresagh <marteresa28@gmail.com>
2021-02-11 09:50:11 +0100

    add new model2graph

--- src/Graph/graph.jl
+++ src/Graph/graph.jl
@@ -1,10 +1,19 @@
-function graph_edge2edge(V::Lar.Points,EV::Lar.Cells)::LightGraphs.SimpleGraphs.SimpleGraph{Int64}
+function model2graph_edge2edge(V::Lar.Points,EV::Lar.Cells)::LightGraphs.SimpleGraphs.SimpleGraph{Int64}
     M1 = Lar.characteristicMatrix(EV)
 	EE = (M1*M1').%2
 	R, C, VAL = Common.findnz(EE)
-	graph = SimpleGraph(size(V,2))
+	graph = SimpleGraph(size(EV,2)) #?? V o EV
 	for i in 1:nnz(EE)
 		add_edge!(graph,R[i],C[i])
+	end
+	return graph
+end
+
+
+function model2graph(V::Lar.Points,EV::Lar.Cells)::LightGraphs.SimpleGraphs.SimpleGraph{Int64}
+	graph = SimpleGraph(size(V,2))
+	for ev in EV
+		add_edge!(graph,ev[1],ev[2])
 	end
 	return graph
 end
