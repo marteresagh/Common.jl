c28af7390b60e5df39d914af1126389ee232c391
marteresagh <marteresa28@gmail.com>
2021-02-19 13:57:15 +0100

    direct graph

--- src/Graph/graph.jl
+++ src/Graph/graph.jl
@@ -17,3 +17,47 @@ function model2graph(V::Lar.Points,EV::Lar.Cells)::LightGraphs.SimpleGraphs.Simp
 	end
 	return graph
 end
+
+
+function makes_direct(V,EV,s)
+	dg = LightGraphs.DiGraph(size(V,2))
+	g = LightGraphs.SimpleGraph(size(V,2))
+	for ev in EV
+		add_edge!(g,ev[1],ev[2])
+	end
+	parents = zeros(Int, nv(g))
+	parents[s] = s
+	seen = zeros(Bool, nv(g))
+	S = Vector{Int}([s])
+	seen[s] = true
+	while !isempty(S)
+		v = S[end]
+		u = 0
+		for n in outneighbors(g, v)
+			if !seen[n]
+				u = n
+				break
+			end
+		end
+		if u == 0
+			pop!(S)
+		else
+			seen[u] = true
+			push!(S, u)
+			parents[u] = v
+			add_edge!(dg,v,u)
+
+		end
+		for n in outneighbors(g, v)
+			if n!=u && parents[v]!=n
+				if seen[n]
+					add_edge!(dg,v,n)
+					break
+				end
+
+			end
+		end
+
+	end
+	return dg
+end
