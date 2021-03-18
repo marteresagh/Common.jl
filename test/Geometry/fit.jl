d485fd13e5717f4810dcf907594fd123bc137cda
marteresagh <marteresa28@gmail.com>
2021-03-18 17:51:38 +0100

    add new fit test

--- test/Geometry/fit.jl
+++ test/Geometry/fit.jl
@@ -1,6 +1,14 @@
 @testset "Fit" begin
 
 	@testset "Fit_Line" begin
+		points = [1. 2. 3. 4.;
+		 		0. 0. 0. 0.]
+
+		params = Common.LinearFit(points)
+		@test params[1] ≈ [1.,0.]
+		@test params[2] ≈ [2.5, 0.]
+
+
 		points = [  1.20728  2.37415  1.58554  1.94907  0.423596  0.364242  2.68154  2.78471  2.4795  2.77911;
 					2.20728  3.37415  2.58554  2.94907  1.4236    1.36424   3.68154  3.78471  3.4795  3.77911]
 		params2D = Common.LinearFit(points)
@@ -8,11 +16,37 @@
 	end
 
 	@testset "Fit_Plane" begin
+		points = [1. 1. 2. 2.;
+		 		1. 2. 1. 2.;
+				0. 0. 0. 0.]
+
+		params = Common.LinearFit(points)
+		@test params[1] ≈ [0.,0.,1.]
+		@test params[2] ≈ [1.5,1.5,0.]
+
+		points = [1. 1. 2. 2.;
+				0. 0. 0. 0.;
+				1. 2. 1. 2.]
+
+		params = Common.LinearFit(points)
+		@test params[1] ≈ [0.,1.,0.]
+		@test params[2] ≈ [1.5,0.,1.5]
+
+		points = [0. 0. 0. 0.;
+				1. 1. 2. 2.;
+				1. 2. 1. 2.]
+
+		params = Common.LinearFit(points)
+		@test params[1] ≈ [1.,0.,0.]
+		@test params[2] ≈ [0.,1.5,1.5]
+
+
 		points = [  2.94039  0.516412   2.29075  0.15646  0.180136   2.39555  0.390856   2.64535  0.804749   2.82538;
 					3.80147  1.99903    1.5376   3.26004  0.162415   1.42049  1.54317    1.97008  2.95325    1.07629;
 				 	17.4241   6.5473    10.9475   7.98946  1.86524   11.0276   5.2589    12.8762   9.32074   11.6287]
-		params3D = Common.LinearFit(points)
-		@test params3D[1] ≈ [0.8017835998182375, 0.5345225852806715, -0.2672614167577797]
+		params = Common.PCA(points)
+		@test Lar.approxVal(4).(params[2][:,3]) ≈ Lar.approxVal(4).([0.8017835998182375, 0.5345225852806715, -0.2672614167577797])
+		@test Lar.approxVal(4).(params[1]) ≈ Lar.approxVal(4).([1.5146032999999999, 1.9723835, 9.488574])
 	end
 
 	@testset "Fit_Circle" begin
