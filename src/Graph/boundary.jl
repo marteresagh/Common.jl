a1dde75ac05593019e4b5c9056410b7627e0a3b4
marteresagh <marteresa28@gmail.com>
2021-03-19 15:25:24 +0100

    boundary extraction: add new fun and test

--- src/Graph/boundary.jl
+++ src/Graph/boundary.jl
@@ -23,7 +23,6 @@ function CV2EV( v )
 		[v[5], v[7]], [v[6], v[8]], [v[1], v[5]], [v[2], v[6]], [v[3], v[7]], [v[4], v[8]]]
 end
 
-
 function get_boundary_edges(V::Lar.Points,FV::Lar.Cells)
 	EV = convert(Array{Array{Int64,1},1}, collect(Set(CAT(map(FV2EV,FV)))))
 
@@ -37,4 +36,31 @@ function get_boundary_edges(V::Lar.Points,FV::Lar.Cells)
 	inner = [k for k=1:length(S1) if S1[k]==2]
 	outer = setdiff(collect(1:length(EV)), inner)
 	return EV[outer]
+end
+
+function get_boundary_points(V::Lar.Points,EV::Lar.Cells)
+	M_2 = Common.K(EV)
+
+	S1 = sum(M_2',dims=2)
+
+	inner = [k for k=1:length(S1) if S1[k]==2]
+	outer = setdiff(union(EV...), inner)
+	return outer
+end
+
+function get_boundary_faces(V::Lar.Points,CV::Lar.Cells)
+	FV = convert(Array{Array{Int64,1},1}, collect(Set(cat(map(CV2FV,CV)))))
+
+	M_2 = K(FV)
+	M_3 = K(CV)
+
+	s = sum(M_2,dims=2)
+	∂_3 = (M_2 * M_3')
+	∂_3 = ∂_3 ./ s
+	∂_3 = ∂_3 .÷ 1	#	.÷ sum(M_2,dims=2)
+
+	S2 = sum(∂_3,dims=2)
+	inner = [k for k=1:length(S2) if S2[k]==2]
+	outer = setdiff(collect(1:length(FV)), inner)
+	return FV[outer]
 end
--- test/Graph/boundary.jl
+++ test/Graph/boundary.jl
@@ -1,13 +1,30 @@
 @testset "Boundary" begin
 
+	@testset "get_boundary_points" begin
+		V = [1. 5. 2. 3. 6. 4. 8. 7.;
+			0. 0. 0. 0. 0. 0. 0. 0.]
+
+		EV = [[1,3],[3,4],[4,6],[6,2],[2,5],[5,8],[8,7]]
+		VV = Common.get_boundary_points(V,EV)
+		@test sort(VV) == [1,7]
+	end
+
 	@testset "get_boundary_edges" begin
 		V = [0. 4. 4. 0. 2. 3. 3. 2;
 			0. 0. 4. 4. 2. 2. 3. 3.]
 
 		FV = [[1, 2, 5],[1, 4, 5],[2, 3, 6],[2, 5, 6],[3, 4, 8],[3, 6, 7],
-			[3, 7, 8],[4, 5, 8],[5, 6, 7],[5, 7, 8]] 
+			[3, 7, 8],[4, 5, 8],[5, 6, 7],[5, 7, 8]]
 		EV = Common.get_boundary_edges(V,FV)
 		@test sort(sort.(EV)) == [[1,2],[1,4],[2,3],[3,4]]
 	end
+
+	@testset "get_boundary_faces" begin
+		V,CV = Lar.cuboidGrid([1,2,1])
+		FV = Common.get_boundary_faces(V,CV)
+		@test sort(sort.(FV)) == [[1, 2, 3, 4],[1, 2, 7, 8],[1, 3, 7, 9],[2, 4, 8, 10],[3, 4, 5, 6],
+		[3, 5, 9, 11],[4, 6, 10, 12],[5, 6, 11, 12],[7, 8, 9, 10],[9, 10, 11, 12]]
+	end
+
 
 end
