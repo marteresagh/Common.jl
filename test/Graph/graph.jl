43a7dab9aa30a532c0f777a934ef570883585e7a
marteresagh <marteresa28@gmail.com>
2021-03-19 15:37:23 +0100

    add new test

--- test/Graph/graph.jl
+++ test/Graph/graph.jl
@@ -15,4 +15,13 @@
 		graph = Common.model2graph(V,EV)
 		@test graph.fadjlist == [[2,4],[1,3],[2,4],[1,3],[6,8],[5,7],[6,8],[5,7]]
 	end
+
+	@testset "makes_direct(g,s)" begin
+		V = [0. 4. 4. 0. 2. 3. 3. 2;
+			0. 0. 4. 4. 2. 2. 3. 3.]
+		EV = [[1,2],[2,6],[6,5],[5,8],[8,7],[7,3],[4,3],[1,4]]
+		g = Common.model2graph(V,EV)
+		graph = Common.makes_direct(g,1)
+		@test graph.fadjlist == [[2],[6],[4],[1],[8],[5],[3],[7]]
+	end
 end
