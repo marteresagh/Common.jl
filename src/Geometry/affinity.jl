27163812bae74cd01814c8f92fdf121b4c4dc0d4
marteresagh <marteresa28@gmail.com>
2021-04-27 15:09:40 +0200

    fix

--- src/Geometry/affinity.jl
+++ src/Geometry/affinity.jl
@@ -1,4 +1,4 @@
-Common."""
+"""
 	t(args::Array{Number,1}...)::Matrix
 
 Return an *affine transformation Matrix* in homogeneous coordinates. Such `translation` Matrix has ``d+1`` rows and ``d+1`` columns, where ``d`` is the number of translation parameters in the `args` array.
